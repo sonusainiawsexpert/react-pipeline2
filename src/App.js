6324dd13147c0d04eca24b5af8d04268828106c7
hiteshsaini33 <sainihitesh33@gmail.com>
2023-07-08 09:02:19 +0530

    app.js changes

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello Diven
+          Hello Suraj
         </p>
         <a
           className="App-link"
